61a8049b2433cf98a2702a6f905b23762d86d1b3
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-06-26 23:16:37 -0700

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DeadLine-Driver-Task-Manager-c/', // must match your repo name
 })
